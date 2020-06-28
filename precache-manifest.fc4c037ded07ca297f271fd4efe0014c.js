e2f1e8501021df62fa449767be3fefe66cac7532
ripudaman <ripudaman.kaushik@yahoo.in>
2020-06-29 00:57:30 +0530

    deploy

---
+++ precache-manifest.fc4c037ded07ca297f271fd4efe0014c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c95673d4bcfc72e5d97433177e9f80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dad139c5051214e7f980",
+    "url": "/static/css/main.0fd6289f.chunk.css"
+  },
+  {
+    "revision": "99c191f2d0d1d51223fc",
+    "url": "/static/js/2.8a3ee324.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8a3ee324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dad139c5051214e7f980",
+    "url": "/static/js/main.c4e12748.chunk.js"
+  },
+  {
+    "revision": "37d381b6aefe6aef1021",
+    "url": "/static/js/runtime-main.5999714a.js"
+  }
+]);
